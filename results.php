d94e71fbab4a2d53e7c9da76c46a892a0781cb5d
Katherine Riker <katherineriker@gmail.com>
2016-11-03 21:30:49 -0400

    OK

--- results.php
+++ results.php
@@ -1,4 +1,16 @@
 <?php
+
+	if (!function_exists('str_putcsv')) {
+        function str_putcsv($input, $delimiter = ',', $enclosure = '"') {
+            $fp = fopen('php://temp', 'r+b');
+            fputcsv($fp, $input, $delimiter, $enclosure);
+            rewind($fp);
+            $data = rtrim(stream_get_contents($fp), "\n");
+            fclose($fp);
+            return $data;
+        }
+    }
+    
     $dbopts = parse_url(getenv('DATABASE_URL'));
     $conn = pg_connect('host='.$dbopts["host"].' port='.$dbopts["port"].' user='.$dbopts["user"].' password='.$dbopts["pass"].' dbname='.ltrim($dbopts["path"],'/'));
     $query = "SELECT * from results;";
@@ -10,9 +22,14 @@
         echo "Error with query: " . $errormessage;
         exit();
     }
+    $csv = '';
     while ($row = pg_fetch_row($result)) {
-        var_dump($row);
+        $part=str_putcsv($row);
+        var_dump($part);
+        $csv .= $part;
     }
+
+    $var_dump($csv);
 
     pg_close($conn);
 ?>
