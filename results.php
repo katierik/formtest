7dcc5d0acc289ab69cde15a3d5ca5af7a8b7b0d1
Katherine Riker <katherineriker@gmail.com>
2016-11-03 21:42:11 -0400

    remove func

--- results.php
+++ results.php
@@ -1,16 +1,4 @@
 <?php
-
-	if (!function_exists('str_putcsv')) {
-        function str_putcsv($input, $delimiter = ',', $enclosure = '"') {
-            $fp = fopen('php://temp', 'r+b');
-            fputcsv($fp, $input, $delimiter, $enclosure);
-            rewind($fp);
-            $data = rtrim(stream_get_contents($fp), "\n");
-            fclose($fp);
-            return $data;
-        }
-    }
-    
     $dbopts = parse_url(getenv('DATABASE_URL'));
     $conn = pg_connect('host='.$dbopts["host"].' port='.$dbopts["port"].' user='.$dbopts["user"].' password='.$dbopts["pass"].' dbname='.ltrim($dbopts["path"],'/'));
     $query = "SELECT * from results;";
